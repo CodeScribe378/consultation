89edbd18f5e7614951776c432fd7de9e1f201676
CodeScribe378 <kristinahor91@gmail.com>
2025-04-15 10:53:39 +0200

    consultation typescript

---
+++ main.ts
@@ -0,0 +1,19 @@
+
+//як типізувати функцію з callback? Дивилась в google, робила подібно
+// //тому що бачила, але нічого з того не працює.
+
+// Array.prototype.forEach = function(callback){
+//     for (const item of this) {
+//         callback(item);
+//     }
+// }
+//
+// Array.prototype.myFilter = function(callback){
+//     let arr: any[] = [];
+//     for (const item of this) {
+//         if (callback(item)) {
+//             arr.push(item);
+//         }
+//     }
+//     return arr;
+// }
